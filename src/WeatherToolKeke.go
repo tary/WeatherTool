ae9028c71e9e033ba26d5c588b27f93e0c719158
Tary Lin <tary.hitwh@gmail.com>
2024-03-12 15:15:14 +0800

    no message

---
+++ src/WeatherToolKeke.go
@@ -0,0 +1,128 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/BurntSushi/toml"
+	"io"
+	"net/http"
+	"os"
+)
+
+type WeatherServer struct {
+	Key       string `toml:"key"`
+	APIUrl    string `toml:"apiUrl"`
+	UnitGroup string `toml:"unitGroup"`
+	Content   string `toml:"content"`
+	ExcelPath string `toml:"excelPath"`
+	SheetName string `toml:"sheetName"`
+}
+
+type Location struct {
+	Location string
+}
+
+type LocationList struct {
+	LocationList []Location
+}
+
+type WeatherPerDayData struct {
+	Datetime string  `toml:"datatime"`
+	TempMax  float32 `toml:"tempmax"`
+	TempMin  float32 `toml:"tempmin"`
+	Humidity float32 `toml:"humidity"`
+}
+
+type WeatherData struct {
+	ResolvedAddress string `toml:"resolvedAddress"`
+
+	Days []WeatherPerDayData `toml:"days"`
+}
+
+func main() {
+	fmt.Println("...小壳天气助手...")
+	fmt.Println("...注意:使用的是免费版天气数据服务每天只能获取1000次数据...")
+	fmt.Println("可以在 https://www.visualcrossing.com/ 注册个新账号,替换Config.toml中的key,获取更多次数.")
+
+	var locationList LocationList
+
+	// Open the TOML file
+	file, err := os.Open("Location.toml")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+
+	// Decode the TOML file into the struct
+	if _, err := toml.NewDecoder(file).Decode(&locationList); err != nil {
+		fmt.Println("Error decoding file:", err)
+		return
+	}
+
+	var serverConfig WeatherServer
+	file2, err2 := os.Open("Config.toml")
+	if err2 != nil {
+		fmt.Println("Error opening file:", err2)
+		return
+	}
+	defer file2.Close()
+
+	// Decode the TOML file into the struct
+	if _, err := toml.NewDecoder(file2).Decode(&serverConfig); err != nil {
+		fmt.Println("Error decoding file:", err)
+		return
+	}
+
+	//https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Harbin%2CCN?unitGroup=metric&key=EB98LKGQRA5HT457J44SC4QBF&contentType=json"
+	urlTemplate := fmt.Sprintf("%sCN%%%%2C%%s?unitGroup=%s&key=%s&contentType=%s",
+		serverConfig.APIUrl, serverConfig.UnitGroup, serverConfig.Key, serverConfig.Content)
+
+	dataList := make([]*WeatherData, 0, len(locationList.LocationList))
+
+	for _, location := range locationList.LocationList {
+		fullUrl := fmt.Sprintf(urlTemplate, location.Location)
+		data := fetchData(fullUrl)
+		if data == nil {
+			return
+		}
+
+		dataList = append(dataList, data)
+		fmt.Printf("成功获取%s的天气数据\n", data.ResolvedAddress)
+	}
+
+	fmt.Printf("开始写入:%s\n", serverConfig.ExcelPath)
+	WriteExcel(serverConfig.ExcelPath, serverConfig.SheetName, dataList)
+	fmt.Printf("执行完成\n")
+}
+
+func fetchData(url string) *WeatherData {
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Error making the request:", err)
+		return nil
+	}
+	defer resp.Body.Close()
+
+	// Check the status code
+	if resp.StatusCode != http.StatusOK {
+		fmt.Println("Error status code:", resp.StatusCode)
+		return nil
+	}
+
+	// Read the response body
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error reading the response body:", err)
+		return nil
+	}
+
+	// Parse the JSON data
+	var data = &WeatherData{}
+	if err := json.Unmarshal(body, data); err != nil {
+		fmt.Println("Error parsing the JSON data:", err)
+		return nil
+	}
+
+	return data
+}
---
+++ src/excelTool.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"fmt"
+	elsxlib "github.com/xuri/excelize/v2"
+	"os"
+	"strconv"
+)
+
+func WriteExcel(fileName, sheetName string, dataList []*WeatherData) {
+	xlFile, err := elsxlib.OpenFile(fileName)
+	if err != nil {
+		if os.IsNotExist(err) {
+			xlFile = elsxlib.NewFile()
+		} else {
+			fmt.Println(err)
+			return
+		}
+	}
+	defer func() {
+		if xlFile != nil {
+			if err := xlFile.Close(); err != nil {
+				fmt.Println(err)
+			}
+		}
+	}()
+
+	const firstRowIndexStr = "1"
+	const secRowIndexStr = "2"
+	const cityColumnStr = "A"
+
+	for cityIdx, cityData := range dataList {
+		//第一行日期, 第二行最低温/最高温
+		cityRowIndexStr := strconv.Itoa(cityIdx + 3)
+		for dayIdx, day := range cityData.Days {
+			minColIdxStr := getColumnIndexStr((dayIdx * 2) + 1)
+			maxColIdxStr := getColumnIndexStr((dayIdx * 2) + 2)
+			//设置日期头
+			if cityIdx == 0 {
+				if dayIdx == 0 && len(sheetName) == 0 {
+					sheetName = day.Datetime + "更新"
+					_, _ = xlFile.NewSheet(sheetName)
+				}
+
+				minColStr := minColIdxStr + firstRowIndexStr
+				maxColStr := maxColIdxStr + firstRowIndexStr
+
+				mergeErr := xlFile.MergeCell(sheetName, minColStr, maxColStr)
+				if mergeErr != nil {
+					fmt.Println(mergeErr)
+				}
+
+				mergeErr = xlFile.SetCellStr(sheetName, minColStr, day.Datetime)
+				mergeErr = xlFile.SetCellStr(sheetName, minColIdxStr+secRowIndexStr, "最低温")
+				mergeErr = xlFile.SetCellStr(sheetName, maxColIdxStr+secRowIndexStr, "最高温")
+				if mergeErr != nil {
+					fmt.Println(mergeErr)
+				}
+			}
+
+			//设置天气数据
+			minDataColStr := minColIdxStr + cityRowIndexStr
+			maxDataColStr := maxColIdxStr + cityRowIndexStr
+			_ = xlFile.SetCellFloat(sheetName, minDataColStr, float64(day.TempMin), 2, 32)
+			_ = xlFile.SetCellFloat(sheetName, maxDataColStr, float64(day.TempMax), 2, 32)
+		}
+
+		_ = xlFile.SetCellStr(sheetName, cityColumnStr+cityRowIndexStr, cityData.ResolvedAddress)
+
+		if cityIdx == 0 {
+			sheetIdx, _ := xlFile.GetSheetIndex(sheetName)
+			xlFile.SetActiveSheet(sheetIdx)
+		}
+	}
+
+	if err := xlFile.SaveAs(fileName); err != nil {
+		fmt.Println(err)
+	}
+}
+
+func getColumnIndexStr(index int) string {
+	if index < 0 {
+		return ""
+	}
+	if index < 26 {
+		return string(rune('A' + index))
+	}
+	return getColumnIndexStr(index/26-1) + getColumnIndexStr(index%26)
+
+}
